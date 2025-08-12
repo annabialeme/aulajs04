d3bd3447547896805b62d8ea262a2b8eb1dcb62b
maria parma <maria.parma@aluno.senai.br>
2025-08-12 09:59:19 -0300

    feat:"update in page"

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,15 +4,4 @@ import Home from "../app/Home/Home";
 export default function Page() {
   return <Home />;
 
-import Link from "next/link";
-
-export default function Home() {
-  return (
-    <main>
-      <Link href="/bibliotecas">
-        <button>Ir para Bibliotecas</button>
-      </Link>
-    </main>
-  );
-
 }
